f614ec57a428bcbc3c986b07726526aab8a5d01a
vishwasarve-cmd <vishwasarve@gmail.com>
2026-02-13 13:16:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+let chart;
+
+function createChart(dataPoints) {
+
+    const ctx = document.getElementById('climateChart').getContext('2d');
+
+    if (chart) chart.destroy();
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['2025','2030','2035','2040','2045','2050'],
+            datasets: [{
+                label: 'Projected Temperature Rise (°C)',
+                data: dataPoints,
+                borderColor: '#00c853',
+                borderWidth: 3,
+                fill: false,
+                tension: 0.3
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    labels: { color: 'white' }
+                }
+            },
+            scales: {
+                x: { ticks: { color: 'white' }},
+                y: { ticks: { color: 'white' }}
+            }
+        }
+    });
 }
 
-const slider = document.getElementById("energySlider");
-const result = document.getElementById("simulationResult");
+function updateSimulation() {
 
-slider.addEventListener("input", function() {
-    let value = slider.value;
+    let baseTemp = parseFloat(document.getElementById("city").value);
 
-    // simple simulation formula
-    let tempRise = (4 - value / 40).toFixed(1);
-    let carbonReduction = (value * 0.65).toFixed(0);
+    let reduction = 0;
 
-    result.innerHTML =
-        "Future Temperature Rise: +" + tempRise + "°C <br>" +
-        "Projected Carbon Reduction: -" + carbonReduction + "%";
-});
+    if (solar.checked) reduction += 0.4;
+    if (metro.checked) reduction += 0.3;
+    if (trees.checked) reduction += 0.5;
+    if (tax.checked) reduction += 0.2;
+
+    let finalTemp = (baseTemp - reduction).toFixed(2);
+
+    document.getElementById("temp").innerText = finalTemp + " °C";
+
+    let score = Math.max(0, Math.min(100, 100 - (finalTemp * 20)));
+    document.getElementById("scoreValue").innerText = score;
+
+    let degree = score * 3.6;
+    document.getElementById("scoreCircle").style.background =
+        `conic-gradient(#00c853 ${degree}deg, #1e293b ${degree}deg)`;
+
+    let projection = [
+        1.2,
+        1.8,
+        2.4,
+        finalTemp - 0.5,
+        finalTemp - 0.2,
+        parseFloat(finalTemp)
+    ];
+
+    createChart(projection);
+}
+
+updateSimulation();
+
 
 
 
